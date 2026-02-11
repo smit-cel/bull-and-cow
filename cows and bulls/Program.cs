7c1c6ffd1339b9bb55ca2cc7dc02af26e9d06969
Smit Shah <23SShah@heckgrammar.co.uk>
2026-02-11 11:22:49 +0000

    fdgb

--- cows and bulls/Program.cs
+++ cows and bulls/Program.cs
@@ -8,99 +8,109 @@ namespace cows_and_bulls
     {
         static void Main(string[] args)
         {
-
-            string choose_number()
+            bool playing = true;
+            while (playing)
             {
-
-                int x = 0;
-                int y = 0;
-                int w = 0;
-                int z = 0;
-                Random random = new Random();
-                x = random.Next(10);
-                y = random.Next(10);
-                z = random.Next(10);
-                w = random.Next(10);
-                while (x == 0)
+                Console.WriteLine("welcome to cows and bulls");
+                string choose_number()
                 {
+                    int x = 0;
+                    int y = 0;
+                    int w = 0;
+                    int z = 0;
+                    Random random = new Random();
                     x = random.Next(10);
-                }
-                while (y == x)
-                {
                     y = random.Next(10);
-                }
-                while (z == y || z == x)
-                {
                     z = random.Next(10);
-                }
-                while (w == x || w == y || w == z)
-                {
                     w = random.Next(10);
-                }
-                string x1 = Convert.ToString(x);
-                string y1 = Convert.ToString(y);
-                string z1 = Convert.ToString(z);
-                string w1 = Convert.ToString(w);
-                return x1 + y1 + z1 + w1;
-            }
-
-            string secret = choose_number();
-            char[] comp = secret.ToCharArray();
-            int bulls = 0;
-            int cows = 0;
-            char[] digits = { '1', '2', '3', '4'};
-            int tries = 0;
-            while (bulls != 4)
-            {
-                cows = 0;
-                bulls = 0;
-                string user = Console.ReadLine();
-                tries ++;
-                digits = user.ToCharArray();
-                bool isInt = int.TryParse(user, out _);
-                if (isInt == false)
-                {
-                    Console.WriteLine("enter numbers only");
-                    break;
-                }
-                if (digits.Length != 4)
-                {
-                    Console.WriteLine("have to enter 4 digits");
-                    break;
-                }
-                if (digits[0] == '0')
-                {
-                    Console.WriteLine("cant have 0 start");
-                    break;
-                }
-                for (int i = 0; i < digits.Length; i++)
-                {
-                    for (int j = i + 1; j < digits.Length; j++)
+                    while (x == 0)
                     {
-                        if (digits[i] == digits[j])
+                        x = random.Next(10);
+                    }
+                    while (y == x)
+                    {
+                        y = random.Next(10);
+                    }
+                    while (z == y || z == x)
+                    {
+                        z = random.Next(10);
+                    }
+                    while (w == x || w == y || w == z)
+                    {
+                        w = random.Next(10);
+                    }
+                    string x1 = Convert.ToString(x);
+                    string y1 = Convert.ToString(y);
+                    string z1 = Convert.ToString(z);
+                    string w1 = Convert.ToString(w);
+                    return x1 + y1 + z1 + w1;
+                }
+
+                string secret = choose_number();
+                char[] comp = secret.ToCharArray();
+                int bulls = 0;
+                int cows = 0;
+                char[] digits = { '1', '2', '3', '4' };
+                int tries = 0;
+                while (bulls != 4)
+                {
+                    cows = 0;
+                    bulls = 0;
+                    string user = Console.ReadLine();
+                    tries++;
+                    digits = user.ToCharArray();
+                    bool isInt = int.TryParse(user, out _);
+                    if (isInt == false)
+                    {
+                        Console.WriteLine("enter numbers only");
+                        break;
+                    }
+                    if (digits.Length != 4)
+                    {
+                        Console.WriteLine("have to enter 4 digits");
+                        break;
+                    }
+                    if (digits[0] == '0')
+                    {
+                        Console.WriteLine("cant have 0 start");
+                        break;
+                    }
+                    for (int i = 0; i < digits.Length; i++)
+                    {
+                        for (int j = i + 1; j < digits.Length; j++)
                         {
-                            Console.WriteLine("cant have any same digits");
-                            goto here;
+                            if (digits[i] == digits[j])
+                            {
+                                Console.WriteLine("cant have any same digits");
+                                goto here;
+                            }
                         }
                     }
+                here:
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (digits[i] == secret[i])
+                        {
+                            bulls++;
+                        }
+                        else if (secret.Contains(digits[i]))
+                        {
+                            cows++;
+                        }
+                    }
+                    Console.WriteLine(cows + " cows " + bulls + " bulls ");
                 }
-            here:
-                for (int i = 0; i < 4; i++)
+                if (bulls == 4)
                 {
-                    if (digits[i] == secret[i])
-                    {
-                        bulls++;
-                    }
-                    else if (secret.Contains(digits[i]))
-                    {
-                        cows++;
-                    }
+                    Console.WriteLine("DONE " + tries + " tries");
                 }
-                Console.WriteLine(cows + " cows " + bulls + " bulls ");
-            }
-            if (bulls == 4)
-            {
-                Console.WriteLine("DONE " + tries + " tries");
+                Console.WriteLine("press enter to play again");
+                playing = false;
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    playing = true;
+                }
             }
         }
     }
